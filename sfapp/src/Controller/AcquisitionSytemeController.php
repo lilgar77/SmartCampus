3230ff8dd6c0dc273587fe6679c54a5b51f05e7d
Gabrielle <gabrielle.ciesla@etudiant.univ-lr.fr>
2024-11-08 12:01:40 +0100

    add of the app fixture and begining of the function and route to list

--- sfapp/src/Controller/AcquisitionSytemeController.php
+++ sfapp/src/Controller/AcquisitionSytemeController.php
@@ -8,11 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AcquisitionSytemeController extends AbstractController
 {
-    #[Route('/acquisition/syteme', name: 'app_acquisition_syteme')]
+    #[Route('/acquisitionsyteme', name: 'app_acquisition_syteme')]
     public function index(): Response
     {
         return $this->render('acquisition_syteme/index.html.twig', [
             'controller_name' => 'AcquisitionSytemeController',
         ]);
     }
+    #[Route('/acquisitionsyteme/list', name: 'list_acquisition_syteme')]
+    public function listAcquisitionSysteme(): Response
+    {
+        return $this->render('acquisition_syteme/index.html.twig', [
+            'controller_name' => 'AcquisitionSytemeController',
+        ]);
+    }
+
+
+
 }
