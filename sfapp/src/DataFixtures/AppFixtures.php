3e4e12e762845ad9ff3108776b07908bd8fd7801
Lilian Garcia (lilgar77) <119511417+lilgar77@users.noreply.github.com>
2024-11-13 17:14:58 +0100

    Add: add game test for fixtures

--- sfapp/src/DataFixtures/AppFixtures.php
+++ sfapp/src/DataFixtures/AppFixtures.php
@@ -7,6 +7,8 @@ use Doctrine\Persistence\ObjectManager;
 use App\Entity\Room;
 use App\Entity\AcquisitionSystem;
 use App\Model\EtatAS;
+use App\Entity\Building;
+use App\Entity\Floor;
 
 class AppFixtures extends Fixture
 {
@@ -20,7 +22,6 @@ class AppFixtures extends Fixture
         $acquisitionSystem->setWording('Salle de réunion');
         $acquisitionSystem->setMacAdress('00:00:00:00:00:00');
         $acquisitionSystem->setEtat(EtatAS::AVAILABLE);
-
         $manager->persist($acquisitionSystem);
 
         $acquisitionSystem2 = new AcquisitionSystem();
@@ -30,14 +31,48 @@ class AppFixtures extends Fixture
         $acquisitionSystem2->setWording('Salle de réunion 2');
         $acquisitionSystem2->setMacAdress('00:00:00:00:00:01');
         $acquisitionSystem2->setEtat(EtatAS::INSTALL);
-
         $manager->persist($acquisitionSystem2);
+
+        $building = new Building();
+        $building->setNameBuilding('Informatique');
+        $building->setAdressBuilding('LaRochelle');
+        $manager->persist($building);
+
+        $building2 = new Building();
+        $building2->setNameBuilding('Tech de co');
+        $building2->setAdressBuilding('LaRochelle');
+        $manager->persist($building2);
+
+
+        $floor1 = new Floor();
+        $floor1->setNumberFloor(0);
+        $floor1->setIdBuilding($building);
+        $manager->persist($floor1);
+
+        $floor2 = new Floor();
+        $floor2->setNumberFloor(1);
+        $floor2->setIdBuilding($building);
+        $manager->persist($floor2);
+
+        $floor3 = new Floor();
+        $floor3->setNumberFloor(2);
+        $floor3->setIdBuilding($building);
+        $manager->persist($floor3);
+
+        $floor4 = new Floor();
+        $floor4->setNumberFloor(3);
+        $floor4->setIdBuilding($building);
+        $manager->persist($floor4);
 
         $room = new Room();
         $room->setName('D302');
+        $room->setFloor($floor4);
+        $room->setBuilding($building);
         $room->setIdAS($acquisitionSystem);
 
         $manager->persist($room);
+
+
 
         $manager->flush();
     }
--- sfapp/src/Form/RoomFormType.php
+++ sfapp/src/Form/RoomFormType.php
@@ -3,11 +3,13 @@
 namespace App\Form;
 
 use App\Entity\AcquisitionSystem;
+use App\Entity\Floor;
 use App\Entity\Room;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Building;
 
 class RoomFormType extends AbstractType
 {
@@ -18,6 +20,14 @@ class RoomFormType extends AbstractType
             ->add('id_AS', EntityType::class, [
                 'class' => AcquisitionSystem::class,
                 'choice_label' => 'mac_adress',
+            ])
+            ->add('floor', EntityType::class, [
+                'class' => Floor::class,
+                'choice_label' => 'numberFloor',
+            ])
+            ->add('building', EntityType::class, [
+                'class' => Building::class,
+                'choice_label' => 'NameBuilding',
             ])
         ;
     }
