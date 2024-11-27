866457f1eb314882190f6ae74abf9e2e64a64b20
Lilian Garcia (lilgar77) <119511417+lilgar77@users.noreply.github.com>
2024-11-27 11:18:16 +0100

    fix: fix problem esteban nullllll

--- sfapp/src/Controller/WelcomeController.php
+++ sfapp/src/Controller/WelcomeController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Room;
 use App\Repository\RoomRepository;
 
 class WelcomeController extends AbstractController
@@ -13,25 +12,28 @@ class WelcomeController extends AbstractController
     #[Route('/', name: 'app_welcome')]
     public function index(RoomRepository $roomRepository): Response
     {
+        // Récupération de toutes les salles
+        $rooms = $roomRepository->findAll();
+
         return $this->render('welcome/index.html.twig', [
             'controller_name' => 'WelcomeController',
-            'rooms' => $roomRepository->findAll(),
+            'rooms' => $rooms,
         ]);
     }
 
     #[Route('/{id}', name: 'app_welcome_details')]
     public function details(RoomRepository $roomRepository, int $id): Response
     {
-        // Recherche l'usager par son ID
+        // Recherche d'une salle par son ID
         $room = $roomRepository->find($id);
 
-        // Gérer le cas où l'usager n'est pas trouvé
+        // Gérer le cas où la salle n'est pas trouvée
         if (!$room) {
-            throw $this->createNotFoundException('Salle non trouvé');
+            throw $this->createNotFoundException('Salle non trouvée');
         }
 
         return $this->render('welcome/detail.html.twig', [
-            'room' => $room
+            'room' => $room,
         ]);
     }
 }
