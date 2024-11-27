f2ad13a01e3364842bf1a8f19083f3ad70343325
Lilian Garcia (lilgar77) <119511417+lilgar77@users.noreply.github.com>
2024-11-27 12:11:25 +0100

    update: welcome controller file

--- sfapp/src/Controller/WelcomeController.php
+++ sfapp/src/Controller/WelcomeController.php
@@ -13,7 +13,7 @@ class WelcomeController extends AbstractController
     public function index(RoomRepository $roomRepository): Response
     {
         // Récupération de toutes les salles
-        $rooms = $roomRepository->findAll();
+        $rooms = $roomRepository->findRoomWithAs();
 
         return $this->render('welcome/index.html.twig', [
             'controller_name' => 'WelcomeController',
@@ -24,10 +24,8 @@ class WelcomeController extends AbstractController
     #[Route('/{id}', name: 'app_welcome_details')]
     public function details(RoomRepository $roomRepository, int $id): Response
     {
-        // Recherche d'une salle par son ID
         $room = $roomRepository->find($id);
 
-        // Gérer le cas où la salle n'est pas trouvée
         if (!$room) {
             throw $this->createNotFoundException('Salle non trouvée');
         }
