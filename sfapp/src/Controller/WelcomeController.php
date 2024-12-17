da7f661dd45fa353804157fbc06da16ebe421c33
Lilian Garcia (lilgar77) <119511417+lilgar77@users.noreply.github.com>
2024-12-17 20:42:17 +0100

    Add: capture days added

--- sfapp/src/Controller/WelcomeController.php
+++ sfapp/src/Controller/WelcomeController.php
@@ -7,14 +7,25 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\RoomRepository;
 
+// use services api
+use App\Service\ApiService;
+
 class WelcomeController extends AbstractController
 {
+    private ApiService $apiService;
+
+    public function __construct(ApiService $apiService)
+    {
+        $this->apiService = $apiService;
+    }
+
     #[Route('/', name: 'app_welcome')]
     public function index(RoomRepository $roomRepository): Response
     {
         // Récupération de toutes les salles
         $rooms = $roomRepository->findRoomWithAs();
 
+        // Rendu de la vue
         return $this->render('welcome/index.html.twig', [
             'controller_name' => 'WelcomeController',
             'rooms' => $rooms,
@@ -22,7 +33,7 @@ class WelcomeController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_welcome_details')]
-    public function details(RoomRepository $roomRepository, int $id): Response
+    public function details(RoomRepository $roomRepository, int $id, ApiService $apiService): Response
     {
         $room = $roomRepository->find($id);
 
@@ -30,8 +41,22 @@ class WelcomeController extends AbstractController
             throw $this->createNotFoundException('Salle non trouvée');
         }
 
+        $date1 = (new \DateTime())->format('Y-m-d');
+        $date2 = (new \DateTime('tomorrow'))->format('Y-m-d');
+
+        try {
+            $data = $apiService->getCapturesByInterval($date1, $date2, 1);
+
+            $lastCapture = $apiService->getLastCapture('ESP-011');
+        } catch (\Exception $e) {
+            $data = ['error' => $e->getMessage()];
+            $lastCapture = ['error' => $e->getMessage()];
+        }
+
         return $this->render('welcome/detail.html.twig', [
             'room' => $room,
+            'data' => $data,
+            'lastCapture' => $lastCapture,
         ]);
     }
 }
---
+++ sfapp/src/Service/ApiService.php
@@ -0,0 +1,84 @@
+<?php
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class ApiService
+{
+    private HttpClientInterface $client;
+
+    private $dbname = 'sae34bdl1eq1';
+    private $username = 'l1eq1';
+    private $userpass = 'dicvex-Zofsip-4juqru';
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    public function getCapturesByInterval(string $date1, string $date2, int $page): array
+    {
+        $url = 'https://sae34.k8s.iut-larochelle.fr/api/captures/interval';
+
+        $headers = [
+            'accept'   => 'application/ld+json',
+            'dbname'   => $this->dbname,
+            'username' => $this->username,
+            'userpass' => $this->userpass,
+        ];
+
+        $query = [
+            'date1' => $date1,
+            'date2' => $date2,
+            'page'  => $page,
+        ];
+
+        try {
+            $response = $this->client->request('GET', $url, [
+                'headers' => $headers,
+                'query'   => $query,
+            ]);
+
+            if ($response->getStatusCode() !== 200) {
+                throw new \Exception('Erreur HTTP : ' . $response->getStatusCode());
+            }
+
+            return $response->toArray();
+        } catch (\Exception $e) {
+            throw new \Exception('Erreur lors de la requête : ' . $e->getMessage());
+        }
+    }
+
+    public function getLastCapture(string $nomsa): array
+    {
+        $url = 'https://sae34.k8s.iut-larochelle.fr/api/captures/last';
+
+        $headers = [
+            'accept'   => 'application/ld+json',
+            'dbname'   => $this->dbname,
+            'username' => $this->username,
+            'userpass' => $this->userpass,
+        ];
+
+        $query = [
+            'nomsa' => $nomsa,
+            'limit' => 1,
+            'page'  => 1,
+        ];
+
+        try {
+            $response = $this->client->request('GET', $url, [
+                'headers' => $headers,
+                'query'   => $query,
+            ]);
+
+            if ($response->getStatusCode() !== 200) {
+                throw new \Exception('Erreur HTTP : ' . $response->getStatusCode());
+            }
+
+            return $response->toArray();
+        } catch (\Exception $e) {
+            throw new \Exception('Erreur lors de la requête : ' . $e->getMessage());
+        }
+    }
+}
