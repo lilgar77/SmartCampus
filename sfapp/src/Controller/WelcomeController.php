ee6361212154afda1c2f5daed41c025d7a8c1747
Lilian Garcia (lilgar77) <119511417+lilgar77@users.noreply.github.com>
2024-12-18 17:46:27 +0100

    add: show data finish

--- sfapp/src/Controller/WelcomeController.php
+++ sfapp/src/Controller/WelcomeController.php
@@ -25,38 +25,66 @@ class WelcomeController extends AbstractController
         // Récupération de toutes les salles
         $rooms = $roomRepository->findRoomWithAs();
 
-        // Rendu de la vue
+
+        $getLastCapture = function(string $type) {
+            return $this->apiService->getLastCapture($type)[0] ?? null;
+        };
+
+        $lastCapturetemp = $getLastCapture('temp');
+        $lastCapturehum = $getLastCapture('hum');
+        $lastCaptureco2 = $getLastCapture('co2');
+
+
         return $this->render('welcome/index.html.twig', [
             'controller_name' => 'WelcomeController',
             'rooms' => $rooms,
+            'lastCapturetemp' => $lastCapturetemp,
+            'lastCapturehum' => $lastCapturehum,
+            'lastCaptureco2' => $lastCaptureco2,
         ]);
     }
+
 
     #[Route('/{id}', name: 'app_welcome_details')]
     public function details(RoomRepository $roomRepository, int $id, ApiService $apiService): Response
     {
         $room = $roomRepository->find($id);
-
         if (!$room) {
             throw $this->createNotFoundException('Salle non trouvée');
         }
 
+        $getLastCapture = function(string $type) use ($apiService) {
+            return $apiService->getLastCapture($type)[0] ?? null;
+        };
+
+        $lastCapturetemp = $getLastCapture('temp');
+        $lastCapturehum = $getLastCapture('hum');
+        $lastCaptureco2 = $getLastCapture('co2');
+
         $date1 = (new \DateTime())->format('Y-m-d');
         $date2 = (new \DateTime('tomorrow'))->format('Y-m-d');
 
-        try {
-            $data = $apiService->getCapturesByInterval($date1, $date2, 1);
+        // Fonction pour récupérer les données d'intervalle pour chaque type
+        $getCapturesByInterval = function(string $type) use ($apiService, $date1, $date2) {
+            try {
+                return $apiService->getCapturesByInterval($date1, $date2, $type, 1);
+            } catch (\Exception $e) {
+                return ['error' => $e->getMessage()];
+            }
+        };
 
-            $lastCapture = $apiService->getLastCapture('ESP-011');
-        } catch (\Exception $e) {
-            $data = ['error' => $e->getMessage()];
-            $lastCapture = ['error' => $e->getMessage()];
-        }
+        $dataTemp = $getCapturesByInterval('temp');
+        $dataHum = $getCapturesByInterval('hum');
+        $dataCo2 = $getCapturesByInterval('co2');
 
         return $this->render('welcome/detail.html.twig', [
             'room' => $room,
-            'data' => $data,
-            'lastCapture' => $lastCapture,
+            'dataTemp' => $dataTemp,
+            'dataHum' => $dataHum,
+            'dataCo2' => $dataCo2,
+            'lastCapturetemp' => $lastCapturetemp,
+            'lastCapturehum' => $lastCapturehum,
+            'lastCaptureco2' => $lastCaptureco2,
         ]);
     }
 }
