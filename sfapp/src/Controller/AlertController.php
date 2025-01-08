2419e7f3e0f26b7751eb24f563c8c6184916b581
Guy Esteban <psupguy2023@gmail.com>
2025-01-08 09:16:44 +0100

    Add : add the update function on the Alert controller

--- sfapp/src/Controller/AlertController.php
+++ sfapp/src/Controller/AlertController.php
@@ -2,26 +2,34 @@
 
 namespace App\Controller;
 
+use App\Service\ApiService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\AlertManager;
 use App\Repository\AlertRepository;
-use Psr\Log\LoggerInterface;
+use App\Repository\RoomRepository;
 
 class AlertController extends AbstractController
 {
     private AlertManager $alertManager;
+    private ApiService $apiService;
+
+    private EntityManagerInterface $entityManager;
 
 
 
-    public function __construct(AlertManager $alertManager)
+    public function __construct(AlertManager $alertManager, ApiService $apiService, EntityManagerInterface $entityManager)
     {
         $this->alertManager = $alertManager;
+        $this->apiService = $apiService;
+        $this->entityManager = $entityManager;
     }
     #[Route('/alert', name: 'app_alert')]
-    public function index(AlertRepository $alertRepository): Response
+    public function index(AlertRepository $alertRepository, RoomRepository $roomRepository): Response
     {
+        $this->apiService->updateLastCapturesForRooms($roomRepository, $this->entityManager);
         $this->alertManager->checkAndCreateAlerts();
 
 
