b9f528796b8124843377913c189eb1ca5655d078
simon <simon.metivier@etudiant.univ-lr.fr>
2025-01-10 14:29:53 +0100

    After merge

--- sfapp/src/Controller/WelcomeController.php
+++ sfapp/src/Controller/WelcomeController.php
@@ -64,8 +64,8 @@ class WelcomeController extends AbstractController
                 'room' => $room,
                 'dbname' => $dbname,
                 'lastCaptures' => [
-                    'temp' => $apiService->getLastCapture('temp', $dbname)[0]['valeur'] ?? null,
-                    'hum' => $apiService->getLastCapture('hum', $dbname)[0]['valeur'] ?? null,
+                    'temp' => round($apiService->getLastCapture('temp', $dbname)[0]['valeur'],1) ?? null,
+                    'hum' =>  round($apiService->getLastCapture('hum', $dbname)[0]['valeur'],1) ?? null,
                     'co2' => $apiService->getLastCapture('co2', $dbname)[0]['valeur'] ?? null,
                 ],
             ];
@@ -81,30 +81,52 @@ class WelcomeController extends AbstractController
 
 
     #[Route('/{id}', name: 'app_welcome_details')]
-    public function details(RoomRepository $roomRepository, int $id, ApiService $apiService): Response
+    public function details(RoomRepository $roomRepository, int $id, ApiService $apiService, AlertManager $alertManager, EntityManagerInterface $entityManager): Response
     {
-        $this->alertManager->checkAndCreateAlerts();
+        // Update the last captures for all rooms
+        $apiService->updateLastCapturesForRooms($roomRepository, $entityManager);
+
+        // Check and create alerts if necessary
+        $alertManager->checkAndCreateAlerts();
+
+        // Fetch the room by its ID
         $room = $roomRepository->find($id);
         if (!$room) {
-            throw $this->createNotFoundException('Salle non trouvée');
+            throw $this->createNotFoundException('Room not found');
         }
 
-       //récupération de la base de donnée de la salle
-        $dbname = $roomRepository->getRoomDb($room->getName())['dbname'];
+        // Retrieve the room's database information
+        $roomName = $room->getName();
 
+        if (!is_string($roomName)) {
+            throw $this->createNotFoundException('Room name is invalid or not found.');
+        }
+
+        $roomDbInfo = $roomRepository->getRoomDb($roomName);
+        $dbname = $roomDbInfo['dbname'] ?? null;
+
+
+        // Get the last capture for each type of data (temperature, humidity, CO2)
         $getLastCapture = function(string $type) use ($apiService, $dbname) {
-            return $apiService->getLastCapture($type, $dbname)[0] ?? null;
+            $capture=$apiService->getLastCapture($type, $dbname)[0] ?? null;
+            if (!is_string($dbname)) {
+                return null;
+            }
+            if ($capture) {
+                $capture['valeur'] = round($capture['valeur'], 1); // Arrondir au dixième près
+            }
+            return $capture;
         };
 
         $lastCapturetemp = $getLastCapture('temp');
         $lastCapturehum = $getLastCapture('hum');
         $lastCaptureco2 = $getLastCapture('co2');
 
+        // Define the date range for the data retrieval
         $date1 = (new \DateTime())->format('Y-m-d');
         $date2 = (new \DateTime('tomorrow'))->format('Y-m-d');
 
-
-        // Fonction pour récupérer les données d'intervalle pour chaque type
+        // Function to retrieve captures by interval
         $getCapturesByInterval = function(string $type) use ($apiService, $date1, $date2, $dbname) {
             try {
                 return $apiService->getCapturesByInterval($date1, $date2, $type, 1, $dbname);
@@ -113,10 +135,12 @@ class WelcomeController extends AbstractController
             }
         };
 
-        $dataTemp = $getCapturesByInterval('temp');
-        $dataHum = $getCapturesByInterval('hum');
-        $dataCo2 = $getCapturesByInterval('co2');
+        // Calculate hourly averages and round them
+        $dataTemp = $this->calculateHourlyAverage($getCapturesByInterval('temp'));
+        $dataHum = $this->calculateHourlyAverage($getCapturesByInterval('hum'));
+        $dataCo2 = $this->calculateHourlyAverage($getCapturesByInterval('co2'));
 
+        // Render the details template with the data
         return $this->render('welcome/detail.html.twig', [
             'room' => $room,
             'dataTemp' => $dataTemp,
@@ -127,4 +151,45 @@ class WelcomeController extends AbstractController
             'lastCaptureco2' => $lastCaptureco2,
         ]);
     }
+
+    /**
+     * Groups data by hour and calculates rounded averages.
+     *
+     * @param array $data Array of data points with 'dateCapture' and 'valeur' keys.
+     * @return array Array of hourly averaged data with rounded values.
+     */
+    private function calculateHourlyAverage(array $data): array
+    {
+        $groupedData = [];
+
+        foreach ($data as $item) {
+            if (!isset($item['dateCapture'], $item['valeur'])) {
+                continue; // Ignore invalid data
+            }
+
+            // Extract the hour from the capture date
+            $hour = (new \DateTime($item['dateCapture']))->format('Y-m-d H:00:00');
+
+            // Initialize the group if it doesn't exist
+            if (!isset($groupedData[$hour])) {
+                $groupedData[$hour] = ['sum' => 0, 'count' => 0];
+            }
+
+            // Sum up the values and count the occurrences
+            $groupedData[$hour]['sum'] += $item['valeur'];
+            $groupedData[$hour]['count']++;
+        }
+
+        $averagedData = [];
+        foreach ($groupedData as $hour => $values) {
+            // Calculate the rounded average for each hour
+            $averagedData[] = [
+                'dateCapture' => $hour,
+                'valeur' => round($values['sum'] / $values['count']), // Rounded average
+            ];
+        }
+
+        return $averagedData;
+    }
+
 }
