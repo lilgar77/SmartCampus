6c18f7509c78e47a9ff9e7d866b9b7c3340bb0ff
simon <simon.metivier@etudiant.univ-lr.fr>
2025-01-10 15:12:42 +0100

    Update: fix problems

--- sfapp/src/Controller/WelcomeController.php
+++ sfapp/src/Controller/WelcomeController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\RoomRepository;
 use Symfony\Component\HttpFoundation\Request;
+use  Doctrine\ORM\EntityManagerInterface;
 
 // use services api
 use App\Service\ApiService;
