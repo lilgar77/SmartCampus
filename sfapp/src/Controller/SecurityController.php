45b7e9521ae41ca69142c6ad5b5a1979e967fb81
Gabrielle <gabrielle.ciesla@etudiant.univ-lr.fr>
2025-01-14 13:14:19 +0100

    Fix : the connexion in the code

--- sfapp/src/Controller/SecurityController.php
+++ sfapp/src/Controller/SecurityController.php
@@ -32,6 +32,13 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
+//    #[Route(path: '/403', name: 'app_error_403')]
+//    public function error403(): Response
+//    {
+//        return $this->render('security/eror401.html.twig', [
+//            'message' => 'Accès interdit : vous n\'êtes pas autorisé à accéder à cette ressource.',
+//        ]);
+//    }
 
 
 }
