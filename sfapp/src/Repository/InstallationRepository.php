2c20b042f59460276e31294a704bc66b53c9f715
Lilian Garcia (lilgar77) <119511417+lilgar77@users.noreply.github.com>
2025-01-15 15:45:39 +0100

    Clean : add comments for installation rpository

--- sfapp/src/Repository/InstallationRepository.php
+++ sfapp/src/Repository/InstallationRepository.php
@@ -7,10 +7,17 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
+ * Repository for the Installation entity.
+ *
  * @extends ServiceEntityRepository<Installation>
  */
 class InstallationRepository extends ServiceEntityRepository
 {
+    /**
+     * Constructor to initialize the repository with the ManagerRegistry.
+     *
+     * @param ManagerRegistry $registry The Doctrine ManagerRegistry instance.
+     */
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Installation::class);
