c6b6abd6f1f0452e5993180ed346871666df83a4
Guy Esteban <psupguy2023@gmail.com>
2025-01-15 14:37:47 +0100

    Add : add all the commentary for the floor controller

--- sfapp/src/Controller/FloorController.php
+++ sfapp/src/Controller/FloorController.php
@@ -1,14 +1,15 @@
 <?php
-###############################################################
-##  @Name of file :FloorController.php                       ##
-##  @brief :Controller for the floor.                        ##
-##          Integration of different routes for the floor    ##
-##  @Function :                                              ##
-##      - index (Page that displays floor)                   ##
-##      - add  (Page that adds floor)                        ##
-##      - delete (Page that deletes floor)                   ##
-##      - edit   (Page that edits floor)                     ##
-###############################################################
+##################################################################
+##  @Name of file : FloorController.php                        ##
+##  @brief : Controller for managing floors.                   ##
+##          Handles CRUD operations and interactions for floors ##
+##  @Functions :                                               ##
+##      - index : Displays a list of floors                    ##
+##      - add   : Handles the addition of a new floor          ##
+##      - delete: Handles the deletion of an existing floor    ##
+##      - edit  : Handles the editing of a floor               ##
+##################################################################
+
 namespace App\Controller;
 
 use App\Repository\RoomRepository;
@@ -27,16 +28,24 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class FloorController extends AbstractController
 {
-
+    /**
+     * Displays a list of floors with search functionality.
+     */
     #[IsGranted("ROLE_ADMIN")]
     #[Route('/floor', name: 'app_floor')]
-    public function index(FloorRepository $floorRepository, RoomRepository $roomRepository, ApiService $apiService, AlertManager $alertManager, EntityManagerInterface $entityManager, Request $request): Response
-    {
-
+    public function index(
+        FloorRepository $floorRepository,
+        RoomRepository $roomRepository,
+        ApiService $apiService,
+        AlertManager $alertManager,
+        EntityManagerInterface $entityManager,
+        Request $request
+    ): Response {
+        // Update room captures and check for alerts
         $apiService->updateLastCapturesForRooms($roomRepository, $entityManager);
         $alertManager->checkAndCreateAlerts();
 
-
+        // Create and handle the search form
         $floor = new Floor();
         $form = $this->createForm(SearchFloorType::class, $floor, [
             'method' => 'GET',
@@ -44,74 +53,92 @@ class FloorController extends AbstractController
 
         $form->handleRequest($request);
 
-        $floorSearch=$floorRepository->findFloorByBuilding($floor);
+        $floorSearch = $form->isSubmitted() && $form->isValid()
+            ? $floorRepository->findFloorByBuilding($floor)
+            : $floorRepository->findFloorByBuilding($floor);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $floorSearch = $floorRepository->findFloorByBuilding($floor);
-        }
-        
         return $this->render('floor/index.html.twig', [
             'floor' => $form->createView(),
             'floors' => $floorSearch,
         ]);
     }
+
+    /**
+     * Handles the addition of a new floor.
+     */
     #[IsGranted("ROLE_ADMIN")]
     #[Route('/floor/add', name: 'app_floor_add')]
-    public function add(Request $request, EntityManagerInterface $entityManager, AlertManager $alertManager): Response
-    {
-
+    public function add(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        AlertManager $alertManager
+    ): Response {
+        // Check and create alerts if necessary
         $alertManager->checkAndCreateAlerts();
 
+        // Create and handle the add floor form
         $floor = new Floor();
         $form = $this->createForm(FloorType::class, $floor);
-
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($floor);
             $entityManager->flush();
 
-            $this->addFlash('success', 'Étage "'. $floor->getNumberFloor() . '" ajouté avec succès ');
+            $this->addFlash('success', 'Floor "' . $floor->getNumberFloor() . '" added successfully.');
 
             return $this->redirectToRoute('app_floor');
         }
 
-        $floor = $entityManager->getRepository(Floor::class)->findAll();
-
         return $this->render('floor/add.html.twig', [
-            'floor' => $floor,
             'floorForm' => $form->createView(),
         ]);
     }
 
+    /**
+     * Handles the deletion of a floor.
+     */
     #[IsGranted("ROLE_ADMIN")]
     #[Route('/floor/{id}', name: 'app_floor_delete', methods: ['POST'])]
-    public function delete(Floor $floor, EntityManagerInterface $entityManager, AlertManager $alertManager): Response
-    {
-
+    public function delete(
+        Floor $floor,
+        EntityManagerInterface $entityManager,
+        AlertManager $alertManager
+    ): Response {
+        // Check and create alerts if necessary
         $alertManager->checkAndCreateAlerts();
 
+        // Remove the specified floor
         $entityManager->remove($floor);
         $entityManager->flush();
 
-        $this->addFlash('success', 'Étage "'. $floor->getNumberFloor() . '" supprimé avec succès ');
+        $this->addFlash('success', 'Floor "' . $floor->getNumberFloor() . '" deleted successfully.');
 
         return $this->redirectToRoute('app_floor');
     }
 
+    /**
+     * Handles the editing of a floor's details.
+     */
     #[IsGranted("ROLE_ADMIN")]
     #[Route('/floor/{id}/edit', name: 'app_floor_edit')]
-    public function edit(Floor $floor, Request $request, EntityManagerInterface $entityManager, AlertManager $alertManager): Response
-    {
-
+    public function edit(
+        Floor $floor,
+        Request $request,
+        EntityManagerInterface $entityManager,
+        AlertManager $alertManager
+    ): Response {
+        // Check and create alerts if necessary
         $alertManager->checkAndCreateAlerts();
 
+        // Create and handle the edit floor form
         $form = $this->createForm(FloorType::class, $floor);
-
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            $this->addFlash('success', 'Étage "'. $floor->getNumberFloor() . '" modifié avec succès ');
+            $this->addFlash('success', 'Floor "' . $floor->getNumberFloor() . '" updated successfully.');
 
             return $this->redirectToRoute('app_floor');
         }
