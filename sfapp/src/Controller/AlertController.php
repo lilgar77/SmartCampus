667a748ccb83995244168508242c0d75ec0f44cb
Guy Esteban <psupguy2023@gmail.com>
2025-01-15 14:21:43 +0100

    Add : add all the commentary for the alert controller

--- sfapp/src/Controller/AlertController.php
+++ sfapp/src/Controller/AlertController.php
@@ -1,5 +1,12 @@
 <?php
-
+##########################################################################
+##  @Name of file : AlertController.php                                 ##
+##  @brief : Controller for managing alerts.                           ##
+##          Integrates functionalities to view ongoing and resolved    ##
+##          alerts in the system.                                      ##
+##  @Function :                                                        ##
+##      - index (Page that displays active and resolved alerts)        ##
+##########################################################################
 namespace App\Controller;
 
 use App\Service\ApiService;
@@ -12,19 +19,23 @@ use App\Repository\AlertRepository;
 use App\Repository\RoomRepository;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-
 class AlertController extends AbstractController
 {
     #[IsGranted("ROLE_ADMIN")]
     #[Route('/alert', name: 'app_alert')]
     public function index(AlertRepository $alertRepository, RoomRepository $roomRepository, ApiService $apiService, EntityManagerInterface $entityManager, AlertManager $alertManager): Response
     {
+        // Update the last capture data for rooms using the API service
         $apiService->updateLastCapturesForRooms($roomRepository, $entityManager);
+
+        // Check existing conditions and create new alerts if necessary
         $alertManager->checkAndCreateAlerts();
 
-
+        // Render the alert management page with ongoing and resolved alerts
         return $this->render('alert/index.html.twig', [
+            // Fetch alerts that are currently active (no end date)
             'alertsBegin' => $alertRepository->findWithoutDateEnd(),
+            // Fetch alerts that have been resolved (have an end date)
             'alertsEnd' => $alertRepository->findWithDateEnd(),
         ]);
     }
