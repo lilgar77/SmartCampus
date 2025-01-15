6f1fc9fcc9754a2a98fce13efd07ccfe23f1ca9c
Guy Esteban <psupguy2023@gmail.com>
2025-01-15 14:44:06 +0100

    Add : add all the commentary for the security controller

--- sfapp/src/Controller/SecurityController.php
+++ sfapp/src/Controller/SecurityController.php
@@ -1,4 +1,11 @@
 <?php
+##################################################################
+##  @Name of file : SecurityController.php                     ##
+##  @brief : Handles user authentication and logout.           ##
+##  @Functions :                                               ##
+##      - login  : Displays the login page and handles errors  ##
+##      - logout : Handles user logout (managed by firewall)   ##
+##################################################################
 
 namespace App\Controller;
 
@@ -9,28 +16,37 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    /**
+     * Displays the login page and handles authentication errors.
+     *
+     * @param AuthenticationUtils $authenticationUtils Provides authentication error and last username.
+     * @return Response
+     */
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-
-        // get the login error if there is one
-
+        // Retrieve the last authentication error, if any
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
+
+        // Retrieve the last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        // Render the login template with the retrieved data
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
-            'error' => $error
+            'error' => $error,
         ]);
     }
 
-    //se gere automatiquement
+    /**
+     * Handles the user logout.
+     *
+     * This method is intercepted by the logout key in the security firewall.
+     */
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
+        // LogicException is thrown as the method is not meant to be executed directly.
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
-
-
 }
