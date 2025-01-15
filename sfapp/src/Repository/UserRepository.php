c619e1329fc9d796930ff2fa9c9b2e530ec99213
Lilian Garcia (lilgar77) <119511417+lilgar77@users.noreply.github.com>
2025-01-15 15:46:42 +0100

    Clean : add comments for user repository

--- sfapp/src/Repository/UserRepository.php
+++ sfapp/src/Repository/UserRepository.php
@@ -10,17 +10,19 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 
 /**
+ * Repository class for the User entity.
+ *
  * @extends ServiceEntityRepository<User>
  */
-
 class UserRepository extends ServiceEntityRepository
 {
     /**
-     * @param ManagerRegistry $registry
+     * Constructor method for UserRepository.
+     *
+     * @param ManagerRegistry $registry The ManagerRegistry instance used to manage database operations.
      */
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
     }
-
 }
